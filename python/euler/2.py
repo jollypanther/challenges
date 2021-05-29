1a3b0c61c72c38eb9737bf961d84d1b1a7aeff8c
jollypanther <mail.jp.subbu@gmail.com>
2021-05-29 07:33:26 +0100

    1 and 2

---
+++ python/euler/2.py
@@ -0,0 +1,25 @@
+# Problem 2: Find the sum of all the even-valued terms in the Fibonacci 
+# sequence which do not exceed one million.
+
+def fib():
+    x, y = 0, 1
+
+    while True:
+        yield x
+        x, y = y, x + y
+
+
+def even(seq):
+    for i in seq:
+        if not i % 2:
+            yield i
+
+
+def under_1m(seq):
+    for i in seq:
+        if i > 1000000:
+            break
+        yield i
+
+
+print(list(even(under_1m(fib()))))
